dd88f078c6b3915bfee1ab2151e36d4f6785379e
anchal00 <anchal.sharma@ridecell.com>
2022-10-26 00:11:46 +0530

    Create app

--- stackunderflow/stackunderflow/settings.py
+++ stackunderflow/stackunderflow/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'stack_underflow_app',
 ]
 
 MIDDLEWARE = [
