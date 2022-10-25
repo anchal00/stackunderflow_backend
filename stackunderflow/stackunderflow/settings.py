d57dae5b2391d8859d42b98c1ecffc486600e13f
anchal00 <anchal.sharma@ridecell.com>
2022-10-26 00:19:21 +0530

    Read secrets from env

--- stackunderflow/stackunderflow/settings.py
+++ stackunderflow/stackunderflow/settings.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -8,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+uu=@%0vth)_yzq@$t*cvzc5(@yoft!g8mj&kfs-$x+06eld_b'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
