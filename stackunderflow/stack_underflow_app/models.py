806009bc126774da3314f3cfb11c446852e9f78c
anchal00 <anchal.sharma@ridecell.com>
2022-10-26 18:46:50 +0530

    Add profession and dob fields

--- stackunderflow/stack_underflow_app/models.py
+++ stackunderflow/stack_underflow_app/models.py
@@ -18,6 +18,8 @@ class User(AbstractUser):
     first_name = models.CharField('first name', max_length=20, blank=False)
     last_name = models.CharField('last name', max_length=20, blank=True)
     email = models.EmailField('email address', unique=True, blank=False)
+    profession = models.CharField('profession', max_length=50, blank=True)
+    dob = models.DateField('date of birth', help_text='Date of birth in format YYYY-MM-DD', null=True)
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
