a628b1bd9bd4ab3efb67d422cefad97e4b4f83f3
anchal00 <anchal.sharma@ridecell.com>
2022-10-28 19:16:53 +0530

    Implement forms and register custom User model with djadmin

--- stackunderflow/stack_underflow_app/admin.py
+++ stackunderflow/stack_underflow_app/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = User
+
+    list_display = ('username', 'email', 'is_active',
+                    'is_staff', 'is_superuser', 'last_login',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        ('General', {'fields': ('username', 'email', 'password', 'dob')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Login and Joining Info', {'fields': ('last_login', 'date_joined')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'dob')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
