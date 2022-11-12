7575106a079cbd66bb8caab3a83d319d584af869
anchal00 <anchal.sharma@ridecell.com>
2022-11-12 19:41:23 +0530

    Integrate a simple logger

--- stackunderflow/stackunderflow/settings.py
+++ stackunderflow/stackunderflow/settings.py
@@ -129,3 +129,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
