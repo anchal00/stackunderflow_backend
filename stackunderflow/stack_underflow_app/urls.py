b81379face454e3823f142b568c9bd7b49eee45a
anchal00 <anchal.sharma@ridecell.com>
2023-02-05 17:11:16 +0530

    Modify API endpoint for answers

--- stackunderflow/stack_underflow_app/urls.py
+++ stackunderflow/stack_underflow_app/urls.py
@@ -1,4 +1,3 @@
-# from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 from stack_underflow_app.apis.answer_apis import AnswerViewSet
 from stack_underflow_app.apis.comment_apis import (AnswerCommentViewSet,
@@ -8,13 +7,17 @@ from stack_underflow_app.apis.tag_apis import TagViewSet
 
 router = routers.DefaultRouter()
 router.register("questions", QuestionViewSet, basename="questions")
-router.register("answers", AnswerViewSet, basename="answers")
+# router.register("answers", AnswerViewSet, basename="answers")
 router.register("tags", TagViewSet, basename="tags")
 
+# For Question's comments
 ques_comments_router = routers.NestedDefaultRouter(router, "questions", lookup="question")
 ques_comments_router.register("comments", QuestionCommentViewSet, basename="ques_comments")
-
-ans_comments_router = routers.NestedDefaultRouter(router, "answers", lookup="answer")
+# For Question's answers
+ques_ans_router = routers.NestedDefaultRouter(router, "questions", lookup="question")
+ques_ans_router.register("answers", AnswerViewSet, basename="ques_answers")
+# For Answer's comments
+ans_comments_router = routers.NestedDefaultRouter(ques_ans_router, "answers", lookup="answer")
 ans_comments_router.register("comments", AnswerCommentViewSet, basename="ans_comments")
 
-urlpatterns = router.urls + ques_comments_router.urls + ans_comments_router.urls
+urlpatterns = router.urls + ques_comments_router.urls + ques_ans_router.urls + ans_comments_router.urls
